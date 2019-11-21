a36c9a8330ecaa67671c5f6fe01756121fadc976
Emil Delfs Danielsen <edanie18@student.aau.dk>
2019-11-21 11:59:47 +0100

    Best branch EU

---
+++ NorthShoreSurfApp/NorthShoreSurfApp/ViewModels/UserModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NorthShoreSurfApp.ViewModels
+{
+    public class UserModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string firstName;
+        private string lastName;
+        private string phoneNo;
+        private string age;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged(nameof(LastName));
+                }
+            }
+        }
+
+        public string PhoneNo
+        {
+            get { return phoneNo; }
+            set
+            {
+                if (phoneNo != value)
+                {
+                    phoneNo = value;
+                    OnPropertyChanged(nameof(PhoneNo));
+                }
+            }
+        }
+
+        public string Age
+        {
+            get { return age; }
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+    }
+}
