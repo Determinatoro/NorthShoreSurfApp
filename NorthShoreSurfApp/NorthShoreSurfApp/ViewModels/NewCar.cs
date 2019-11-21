e1e0bdc3ad0b3498b3d2bb7fae10f0c4b82e51b9
Emil-Linnebjerg <43955067+Emil-Linnebjerg@users.noreply.github.com>
2019-11-21 09:30:39 +0100

    firs look

---
+++ NorthShoreSurfApp/NorthShoreSurfApp/ViewModels/NewCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthShoreSurfApp.ViewModels
+{
+    public class NewCar
+    {
+        private string licensPlate;
+        private string carColor;
+
+
+
+    }
+}
